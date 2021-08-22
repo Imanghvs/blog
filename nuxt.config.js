54ca204c3217c2fb17db5a0ae505f29521c08cd0
Iman Ghavasieh <iman.ghavasieh@gmail.com>
2021-08-23 01:22:15 +0430

    structure

---
+++ nuxt.config.js
@@ -0,0 +1,43 @@
+export default {
+  // Global page headers
+  head: {
+    title: 'ایمانگرام',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'با ایمانگرام در سه سوت بلاگ بسازید!',
+      },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS
+  css: ['@/assets/global.css'],
+
+  // Plugins to run before rendering page
+  plugins: [],
+
+  // Auto import components
+  components: true,
+
+  // Modules for dev and build
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
+
+  // Modules
+  modules: ['@nuxtjs/axios'],
+
+  // Axios module configuration
+  axios: {},
+
+  // Vuetify module configuration
+  vuetify: {},
+
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">صفحه‌ی اصلی</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .container {
+    direction: rtl;
+  }
+</style>
